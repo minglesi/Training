ee33b6c60f8bdb945b1d4bd4d393968bb5e4afdf
Matias <minglesi@gmail.com>
2023-02-22 14:56:48 -0300

    fixes

--- src/api.py
+++ src/api.py
@@ -6,7 +6,7 @@ import os
 db_pass = os.environ.get('DB_PASS')
 app = Flask(__name__, template_folder="template")
 pymysql.install_as_MySQLdb()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:' + str(db_pass) + '@localhost:3306/demo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:' + str(db_pass) + '@localhost:3307/demo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['EXPLAIN_TEMPLATE_LOADING'] = True
 db = SQLAlchemy(app)
@@ -15,11 +15,12 @@ db = SQLAlchemy(app)
 class Hero(db.Model):
     hero_id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(30), nullable=False)
+    race = db.Column(db.String(10), nullable=False)
     profession_id = db.Column(db.Integer, db.ForeignKey('profession.profession_id'), nullable=False)
     profession = db.relationship("Profession", back_populates="Hero")
 
     def __repr__(self):
-        return '%s, %s' % (self.name, self.profession)
+        return '%s, %s' % (self.name, self.race)
 
 
 class Profession(db.Model):
@@ -30,7 +31,7 @@ class Profession(db.Model):
     mana = db.Column(db.Integer())
 
     def __repr__(self):
-        return '%s, %s, %s' % (self.profession, self.hp, self.mana)
+        return '%s - %s hp, %s mana' % (self.profession, self.hp, self.mana)
 
 
 def validate_hero_profession(hero, profession):
@@ -61,19 +62,116 @@ def invalid_route(e):
 
 @app.route('/')
 def index():
+    db.drop_all()
+    db.create_all()
     return render_template('index.html')
 
 
-@app.route('/profession', methods=['GET', 'POST'])
+@app.route('/hero', methods=['GET'])
+def hero():
+    professions = Profession.query.all()
+    profession_id = request.args.get("profession_id")
+    if profession_id:
+        profession_id = int(profession_id)
+    return render_template('hero.html', professions=professions, profession_id=profession_id)
+
+
+@app.route('/hero', methods=['POST'])
+def add_hero():
+    name = request.form.get("name")
+    race = request.form.get("race")
+    profession_id = request.form.get("profession")
+    new_hero = Hero(name=name, race=race, profession_id=profession_id)
+    #errors = validate_hero_profession(new_hero, new_profession)
+    #if errors:
+    #    return render_template('hero.html', name=Hero.name, profession=Hero.profession, hp=Profession.hp,
+    #                           mana=Profession.mana)
+    try:
+        db.session.add(new_hero)
+        db.session.commit()
+        return redirect(f"/heroes")
+    except Exception as e:
+        return f"There was an error adding data: {e}"
+
+
+@app.route('/hero/<int:hero_id>', methods=['GET'])
+def get_hero(hero_id):
+    hero = Hero.query.get(hero_id)
+    if hero is None:
+        return redirect('/error')
+    else:
+        profession_id = request.args.get("profession_id")
+        if profession_id:
+            profession_id = int(profession_id)
+        else:
+            profession_id = hero.profession_id
+        professions = Profession.query.all()
+        return render_template('hero.html', hero_id=hero_id, name=hero.name, profession_id=profession_id,
+                               race=hero.race, professions=professions)
+
+
+@app.route('/hero/<int:hero_id>', methods=['POST'])
+def update_hero(hero_id):
+    hero = Hero.query.get(hero_id)
+    if hero is None:
+        return redirect('/error')
+    else:
+        hero.profession_id = request.form.get("profession")
+        hero.name = request.form.get("name")
+        hero.race = request.form.get("race")
+        #errors = validate_hero_profession(hero, profession)
+
+        #if errors:
+        #    return render_template('hero.html', name=Hero.name, profession=Hero.profession, hp=Profession.hp,
+        #                           mana=Profession.mana)
+        try:
+            db.session.commit()
+            return redirect("/heroes")
+        except Exception as e:
+            return f"There was an error adding data: {e}"
+
+
+@app.route('/heroes', methods=['GET'])
+def show_heroes():
+    try:
+        heroes = Hero.query.all()
+        return render_template('heroes.html', heroes=heroes)
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+
+@app.route('/hero/<int:hero_id>/delete', methods=['GET', 'POST'])
+def delete_hero(hero_id):
+    hero = Hero.query.get(hero_id)                  ##cuándo elimino el heroe, que pasa con la profession? se elimina
+    db.session.delete(hero)                         ##solo por la relacion que hay entre hero_id y profession_id o no?
+    db.session.commit()
+    return redirect("/heroes")
+
+
+@app.route('/heroes/delete', methods=['GET', 'POST'])
+def delete_all_heroes():
+    db.session.query(Hero).delete()
+    db.session.commit()
+    return redirect("/hero")
+
+
+@app.route('/profession', methods=['POST'])
 def add_profession():
     profession = request.form.get("new_profession")
-    new_profession = Profession(profession=profession)
+    hp = request.form.get("new_profession_hp")
+    mana = request.form.get("new_profession_mana")
+    hero_id = request.args.get("hero_id")
+    new_profession = Profession(profession=profession, hp=hp, mana=mana)
     professions = Profession.query.all()
-    professions_list = [str(profession) for profession in professions]
+    professions_list = [profession.profession for profession in professions]
     if profession not in professions_list:
         db.session.add(new_profession)
     db.session.commit()
-    return redirect("/hero")
+    suffix = f"?profession_id={new_profession.profession_id}"
+    if hero_id:
+        return redirect(f"/hero/{hero_id}" + suffix)
+    else:
+        return redirect("/hero" + suffix)
 
 
 @app.route('/professions', methods=['GET'])
@@ -84,89 +182,6 @@ def show_professions():
     except Exception as e:
         return render_template('error.html', error=e)
 
-
-@app.route('/hero', methods=['GET'])
-def all_heroes():
-    professions = Profession.query.all()
-    return render_template('hero.html', profession=professions)
-
-
-@app.route('/heroes/<int:hero_id>', methods=['GET'])
-def get_hero(hero_id):
-    hero = Hero.query.get(hero_id)
-    if hero is None:
-        return redirect('/error')
-    else:
-        return render_template('hero.html', name=Hero.name, profession=Hero.profession, hp=Profession.hp,
-                               mana=Profession.mana)
-
-
-@app.route('/heroes/<int:hero_id>', methods=['POST'])
-def update_hero_profession(hero_id):
-    hero = Hero.query.get(hero_id)
-    profession = Profession.query.get(hero_id)
-    if hero is None:
-        return redirect('/error')
-    else:
-        Hero.profession_id = request.form.get("profession")
-        Hero.name = request.form.get("name")
-        Profession.hp = request.form.get("hp")
-        Profession.mana = request.form.get("mana")
-        errors = validate_hero_profession(hero, profession)
-
-        if errors:
-            return render_template('hero.html', name=Hero.name, profession=Hero.profession, hp=Profession.hp,
-                                   mana=Profession.mana)
-        try:
-            db.session.commit()
-            return redirect("/hero")
-        except Exception as e:
-            return f"There was an error adding data: {e}"
-
-
-@app.route('/heroes/new', methods=['POST'])
-def add_hero():
-    name = request.form.get("name")
-    profession = request.form.get("profession")
-    hp = request.form.get("hp")
-    mana = request.form.get("mana")
-    new_hero = Hero(name=name)
-    new_profession = Profession(profession=profession, hp=hp, mana=mana)
-    errors = validate_hero_profession(new_hero, new_profession)
-    if errors:
-        return render_template('hero.html', name=Hero.name, profession=Hero.profession, hp=Profession.hp,
-                               mana=Profession.mana)
-    try:
-        db.session.add(new_hero)
-        db.session.add(new_profession)
-        db.session.commit()
-        return redirect("/todos")
-    except Exception as e:
-        return f"There was an error adding data: {e}"
-
-
-@app.route('/heroes', methods=['GET'])
-def show_heroes():
-    try:
-        heroes = list(Hero.query.all)
-        return render_template('heroes.html', heroes=heroes)
-    except Exception as e:
-        return render_template('error.html', error=e)
-
-
-@app.route('/heroes/<int:hero_id>/delete', methods=['GET', 'POST'])
-def delete_hero(hero_id):
-    hero = Hero.query.get(hero_id)                  ##cuándo elimino el heroe, que pasa con la profession? se elimina
-    db.session.delete(hero)                         ##solo por la relacion que hay entre hero_id y profession_id o no?
-    db.session.commit()
-    return redirect("/hero")
-
-
-@app.route('/heroes/delete', methods=['GET', 'POST'])
-def delete_all_heroes():
-    db.session.query(Hero).delete()
-    db.session.commit()
-    return redirect("/hero")
 
 
 if __name__ == '__main__':
